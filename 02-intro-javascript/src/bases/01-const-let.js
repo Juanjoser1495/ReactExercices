f457527e0e7b82cf8f88d3f69be80b8a95eedd36
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-07-18 15:40:50 -0500

    Create app and variables and constants

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+console.log('Hola Mundo');
+
+const nombre = 'Juan Jose';
+const apellido = 'Rojas';
+
+let valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    let valorDado = 6;
+    console.log(valorDado);
+}
+
+console.logv(valorDado);
+
