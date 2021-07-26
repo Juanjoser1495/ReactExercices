ff9ddcd645270123c12c1ca96255a9632ff6731e
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-07-26 17:29:40 -0500

    async & await and operator 3

---
+++ 02-intro-javascript/src/bases/11-async-await.js
@@ -0,0 +1,19 @@
+const getImagenPromesa = () =>
+  new Promise((resolve) => resolve("https://asfadsf.com"));
+getImagenPromesa().then(console.log);
+
+const getImagen = async () => {
+  const apiKey = "MYd3MEMyY4Z5fG1JiX7Rer2gRn8rFU3u";
+  const url = "http://api.giphy.com/v1/gifs/random?api_key=";
+
+  const resp = await fetch(url + apiKey);
+  const data = await resp.json();
+  return data;
+};
+
+getImagen().then(({ data }) => {
+  const { url } = data.images.original;
+  const img = document.createElement("img");
+  img.src = url;
+  document.body.append(img);
+});
