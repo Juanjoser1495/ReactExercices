16ed59156e3234f15476457815d793dccf7273af
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-08-05 17:05:55 -0500

    DefaultProps

--- 03-counter-app/src/PrimerApp.js
+++ 03-counter-app/src/PrimerApp.js
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 
-const PrimeraApp = ({ saludo }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
   const saludo2 = "Hola Mundo const";
 
   return (
     <>
       <h1>{saludo2}</h1>
       <h2>{saludo}</h2>
+      <h3>{subtitulo}</h3>
       <p>Mi primera app</p>
     </>
   );
@@ -14,6 +15,10 @@ const PrimeraApp = ({ saludo }) => {
 
 PrimeraApp.propTypes = {
   saludo: PropTypes.string.isRequired,
+};
+
+PrimeraApp.defaultProps = {
+  subtitulo: "Soy un subtitulo",
 };
 
 export default PrimeraApp;
