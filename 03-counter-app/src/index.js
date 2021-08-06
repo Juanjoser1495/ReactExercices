aa43f9e86b93424f78a82f7f1a9b9a30097e1b0c
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-08-06 09:46:17 -0500

    CounterApp homework

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PrimeraApp from "./PrimerApp";
 import "./index.css";
+import CounterApp from "./CounterApp";
 
 const divRoot = document.querySelector("#root");
 
-ReactDOM.render(<PrimeraApp saludo="Hola, soy goku" />, divRoot);
+ReactDOM.render(<CounterApp value={10} />, divRoot);
