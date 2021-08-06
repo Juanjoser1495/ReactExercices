a52e47a40054d44839008af3520b2faa7bc1ed94
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-08-06 11:30:59 -0500

    Events

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,10 +1,21 @@
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+  //handleAdd
+  const handleAdd = (e) => {
+    console.log(e);
+  };
   return (
     <div>
       <h1>CounterApp</h1>
       <h2>{value}</h2>
+      <button
+        onClick={(e) => {
+          handleAdd(e);
+        }}
+      >
+        +1
+      </button>
     </div>
   );
 };
