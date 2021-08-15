8d534aeaba568ff41926cda79272db9cc760030f
jrojasvelez <jrojasvelez@deloitte.com>
2021-08-15 16:24:42 -0500

    First test with assertions

--- 03-counter-app/src/tests/demo.test.js
+++ 03-counter-app/src/tests/demo.test.js
@@ -1,3 +1,10 @@
- test('FirstTest', () =>{
-     
- })
+describe('SuiteDePruebasDemo', () => {
+    test("FirstTest", () => {
+        const mensaje = "Hola Mundo";
+    
+        const mensaje2 = `Hola Mundo`;
+    
+        expect(mensaje).toBe(mensaje2);
+    });
+});
+
