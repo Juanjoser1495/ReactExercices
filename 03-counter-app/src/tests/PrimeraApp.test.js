7a02b8ceb2796ead8c280ff64e3ffaa76694f296
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-08-26 16:58:24 -0500

    Learning unit test with components

---
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import PrimeraApp from "../PrimerApp";
+
+describe("Pruebas en <PrimeraApp />", () => {
+  test("Debe de mostrar el mensaje 'Hola Soy Goku'", () => {
+    const saludo = "Hola Soy Goku";
+
+    const { getByText } = render(<PrimeraApp saludo={saludo}></PrimeraApp>);
+    expect(getByText(saludo)).toBeInTheDocument();
+  });
+});
