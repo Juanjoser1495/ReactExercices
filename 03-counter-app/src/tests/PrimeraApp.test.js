1b2c585d03f4d2f47f3ce4f5fb6888c561c3ff28
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-08-27 16:01:45 -0500

    First unit test with enzyme

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,4 +1,5 @@
 import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
 import PrimeraApp from "../PrimerApp";
 
 describe("Pruebas en <PrimeraApp />", () => {
@@ -7,4 +8,10 @@ describe("Pruebas en <PrimeraApp />", () => {
   //     const { getByText } = render(<PrimeraApp saludo={saludo}></PrimeraApp>);
   //     expect(getByText(saludo)).toBeInTheDocument();
   //   });
+
+  test("Debe de mostrar el mensaje 'Hola Soy Goku'", () => {
+    const saludo = "Hola Soy Goku";
+    const wrapper = shallow(<PrimeraApp saludo={saludo}></PrimeraApp>);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
