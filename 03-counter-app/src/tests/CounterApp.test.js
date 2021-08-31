9e8676d45e6a890a2e79c7021b65337746ce64d3
jrojasvelez <jrojasvelez@deloitte.com>
2021-08-31 17:59:10 -0500

    Simulating events on tests

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -2,8 +2,12 @@ import { shallow } from "enzyme";
 import CounterApp from "../CounterApp";
 
 describe("Pruebas en <CounterApp />", () => {
+  let wrapper = shallow(<CounterApp></CounterApp>);
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp></CounterApp>);
+  });
+
   test("Debe de mostrar el <CounterApp> correctamnete", () => {
-    const wrapper = shallow(<CounterApp></CounterApp>);
     expect(wrapper).toMatchSnapshot();
   });
 
@@ -12,5 +16,17 @@ describe("Pruebas en <CounterApp />", () => {
     const wrapper = shallow(<CounterApp value={100}></CounterApp>);
     const returned = wrapper.find("h3").text();
     expect(returned).toBe(value);
+  });
+
+  test("Debe incrementar con el boton +1", () => {
+    wrapper.find("button").at(1).simulate("click");
+    const returned = wrapper.find("h3").text();
+    expect(returned).toBe("1");
+  });
+
+  test("Debe reducir con el boton -1", () => {
+    wrapper.find("button").at(3).simulate("click");
+    const returned = wrapper.find("h3").text();
+    expect(returned).toBe("-1");
   });
 });
