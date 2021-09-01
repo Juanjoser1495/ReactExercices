ebf201d39dcb663991ab86042ae57fbe2dd34344
jrojasvelez <jrojasvelez@deloitte.com>
2021-09-01 15:23:30 -0500

    adding the categories hook to add a category

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,8 +1,27 @@
+import { useState } from "react";
+
 const GifExpertApp = () => {
+  //const categories = ["Dragon Ball", "Pokemon", "Samurai X"];
+  const [categories, setCategories] = useState([
+    "Dragon Ball",
+    "Pokemon",
+    "Samurai X",
+  ]);
+
+  const handleAdd = () => {
+    setCategories([...categories, "Hunter X"]);
+  };
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr></hr>
+
+      <button onClick={handleAdd}>Add</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
