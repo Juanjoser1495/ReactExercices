4191f853b72dd08df3b097735025400443851c01
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-09-02 14:27:05 -0500

    Communication between components

--- 04-gif-expert-app/src/components/AddCategory.js
+++ 04-gif-expert-app/src/components/AddCategory.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 
 const AddCategory = ({ setCategories }) => {
-  const [inputValue, setInputValue] = useState();
+  const [inputValue, setInputValue] = useState("");
   const handleOnChange = (e) => {
     setInputValue(e.target.value);
   };
