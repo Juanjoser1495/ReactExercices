81dee14d2ea19b2a2bb835082088b695fd68f477
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-09-13 09:25:13 -0500

    Adding proptypes and updating the test suite

--- 04-gif-expert-app/src/test/components/GigGridItem.test.js
+++ 04-gif-expert-app/src/test/components/GigGridItem.test.js
@@ -2,8 +2,10 @@ import { shallow } from "enzyme";
 import GifGridItem from "../../components/GifGridItem";
 
 describe("Pruebas en <GifGridItem />", () => {
+  const title = "A title";
+  const url = "https://localhost/algo.jpg";
   test("Debe de mostrar el <GifGridItem> correctamnete", () => {
-    let wrapper = shallow(<GifGridItem></GifGridItem>);
+    let wrapper = shallow(<GifGridItem title={title} url={url}></GifGridItem>);
     expect(wrapper).toMatchSnapshot();
   });
 });
