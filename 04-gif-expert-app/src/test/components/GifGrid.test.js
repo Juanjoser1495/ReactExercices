5e65b8b799211756bc7877721e6d7ace8a48d8d6
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-09-16 17:29:19 -0500

    adding test to gifgrid component

---
+++ 04-gif-expert-app/src/test/components/GifGrid.test.js
@@ -0,0 +1,23 @@
+import { shallow } from "enzyme";
+import GifGrid from "../../components/GifGrid";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+jest.mock("../../hooks/useFetchGifs");
+
+describe("Pruebas en <GifGrid />", () => {
+  const category = "Dragon ball";
+
+  test("Debe de mostrar el <GifGrid> correctamnete", () => {
+    useFetchGifs.mockReturnValue({ data: [], loading: true });
+    const wrapper = shallow(<GifGrid category={category}></GifGrid>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("Debe de mostrar items cuando se cargan imagenes useFetchGifs", () => {
+    const imgs = [
+      { id: "abc", url: "https://localhost/cosa.jpg", title: "Cualquier cosa" },
+    ];
+    useFetchGifs.mockReturnValue({ data: imgs, loading: false });
+    const wrapper = shallow(<GifGrid category={category}></GifGrid>);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
