b85bb342a2c7fabb18adb530534505379eccc9a1
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-09-16 16:41:44 -0500

    Creating our first mock in setCategories Function

--- 04-gif-expert-app/src/test/components/AddCategory.test.js
+++ 04-gif-expert-app/src/test/components/AddCategory.test.js
@@ -2,10 +2,17 @@ import { shallow } from "enzyme";
 import AddCategory from "../../components/AddCategory";
 
 describe("Pruebas en <AddCategory />", () => {
-  const setCategories = () => {};
-  const wrapper = shallow(
+  const setCategories = jest.fn();
+  let wrapper = shallow(
     <AddCategory setCategories={setCategories}></AddCategory>
   );
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    wrapper = shallow(
+      <AddCategory setCategories={setCategories}></AddCategory>
+    );
+  });
 
   test("Debe de mostrar el <AddCategory> correctamnete", () => {
     expect(wrapper).toMatchSnapshot();
@@ -16,5 +23,10 @@ describe("Pruebas en <AddCategory />", () => {
     const value = "Hola Mundo";
     input.simulate("change", { target: { value } });
     expect(wrapper.find("p").text().trim()).toBe(value);
+  });
+
+  test("No debe de postear info onsubmit", () => {
+    wrapper.find("form").simulate("submit", { preventDefault: () => {} });
+    expect(setCategories).not.toHaveBeenCalled();
   });
 });
