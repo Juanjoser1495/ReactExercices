3c382e5470d1f58bdbe440d61e9716478034bf2b
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-10-04 17:16:03 -0500

    Mixing the two custom hooks

--- 05-hooks-app/src/components/03-examples/MultipleCustomHooks.js
+++ 05-hooks-app/src/components/03-examples/MultipleCustomHooks.js
@@ -1,12 +1,30 @@
+import { useCounter } from "../../hooks/useCounter";
 import { useFetch } from "../../hooks/useFetch";
 import "../02-useEffect/effects.css";
 
 export const MultipleCustomHooks = () => {
-  const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-  console.log(state);
+  const { state, increment } = useCounter(1);
+  const { loading, data, error } = useFetch(
+    `https://www.breakingbadapi.com/api/quotes/${state}`
+  );
+
+  const { author, quote } = !!data && data[0];
+
   return (
     <div>
-      <h1>Custom Hooks!!</h1>
+      <h1>Breakng bad Quotes</h1>
+      <hr></hr>
+      {loading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-right">
+          <p className="mb-0">{quote}</p>
+          <footer className="blockquote-footer">{author}</footer>
+        </blockquote>
+      )}
+      <button className="btn btn-primary" onClick={() => increment(1)}>
+        Siguiente quote
+      </button>
     </div>
   );
 };
