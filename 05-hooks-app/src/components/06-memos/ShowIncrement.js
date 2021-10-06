094a054374b536b5e31021c052b22fcc5e37224d
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2021-10-06 16:12:52 -0500

    use call back hook

---
+++ 05-hooks-app/src/components/06-memos/ShowIncrement.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const ShowIncrement = React.memo(({ increment }) => {
+  console.log("Me volvi a generar");
+  return (
+    <button
+      onClick={() => {
+        increment(5);
+      }}
+    >
+      +1
+    </button>
+  );
+});
