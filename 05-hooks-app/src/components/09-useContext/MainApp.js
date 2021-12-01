5e1f16a4ac9d1e16a01fb6e04ab97f4f08892274
Juanjoser14 <juanjoserojasv@gmail.com>
2021-12-01 15:57:10 -0500

    Creating the context

--- 05-hooks-app/src/components/09-useContext/MainApp.js
+++ 05-hooks-app/src/components/09-useContext/MainApp.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { AppRouter } from "./AppRouter";
+import { UserContext } from "./UserContext";
 
 export const MainApp = () => {
-  return <AppRouter />;
+  const user = { id: 1234, name: "Fernando", email: "juan@gmail.com" };
+  return (
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
 };
