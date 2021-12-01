868c17f537b789e879ffa72f2a1a3eefca54a3c5
Juanjoser14 <juanjoserojasv@gmail.com>
2021-12-01 09:03:14 -0500

    Adding the first router

--- 05-hooks-app/src/index.js
+++ 05-hooks-app/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { TodoApp } from "./components/08-useReducer/TodoApp";
+import { MainApp } from "./components/09-useContext/MainApp";
+// import { TodoApp } from "./components/08-useReducer/TodoApp";
 //import { Padre } from "./components/07-tarea-memo/Padre";
 //import { CallbackHook } from "./components/06-memos/CallbackHook";
 //import { MemoHook } from "./components/06-memos/MemoHook";
@@ -15,6 +16,6 @@ import { TodoApp } from "./components/08-useReducer/TodoApp";
 //import { CounterApp } from "./components/01-useState/CounterApp";
 //import { HookApp } from "./HookApp";
 
-ReactDOM.render(<TodoApp />, document.getElementById("root"));
+ReactDOM.render(<MainApp />, document.getElementById("root"));
 
 //import "./components/08-useReducer/intro-reducer";
