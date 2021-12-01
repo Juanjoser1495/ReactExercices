144e391b62715f847d5532aa7bc63753ea1f9989
Juanjoser14 <juanjoserojasv@gmail.com>
2021-12-01 10:16:19 -0500

    Adding link and navlink components

--- 05-hooks-app/src/components/09-useContext/AppRouter.js
+++ 05-hooks-app/src/components/09-useContext/AppRouter.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AboutScreen } from "./AboutScreen";
 import { LoginScreen } from "./LoginScreen";
 import { HomeScreen } from "./HomeScreen";
+import { NavBar } from "./NavBar";
 
 export const AppRouter = () => {
   return (
     <Router>
       <div>
+        <NavBar />
         <Routes>
           <Route exact path="/" element={<HomeScreen />} />
           <Route exact path="/about" element={<AboutScreen />} />
