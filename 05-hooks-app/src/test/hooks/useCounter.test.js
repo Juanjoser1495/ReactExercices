853e8094d99cd0e434908c5f8e99c765fe7073ce
Juanjoser14 <juanjoserojasv@gmail.com>
2021-12-16 17:06:20 -0500

    Testing the functions of the custom hook

--- 05-hooks-app/src/test/hooks/useCounter.test.js
+++ 05-hooks-app/src/test/hooks/useCounter.test.js
@@ -1,5 +1,5 @@
 import { useCounter } from "../../hooks/useCounter";
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 
 describe("Pruebas en useCounter", () => {
   test("Debe de retornar valores por defecto", () => {
@@ -10,5 +10,45 @@ describe("Pruebas en useCounter", () => {
     expect(typeof increment).toBe("function");
     expect(typeof decrement).toBe("function");
     expect(typeof reset).toBe("function");
+  });
+
+  test("Debe de retornar el valor enviado por parametro", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { state } = result.current;
+    expect(state).toBe(100);
+  });
+
+  test("Debe de retornar el valor incrementado en 1", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { increment } = result.current;
+    act(() => {
+      increment();
+    });
+
+    const { state } = result.current;
+    expect(state).toBe(101);
+  });
+
+  test("Debe de retornar el valor decrementar en 1", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { decrement } = result.current;
+    act(() => {
+      decrement();
+    });
+
+    const { state } = result.current;
+    expect(state).toBe(99);
+  });
+
+  test("Debe de retornar el valor reseteado luego de la incrementacion en 1", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { increment, reset } = result.current;
+    act(() => {
+      increment();
+      reset();
+    });
+
+    const { state } = result.current;
+    expect(state).toBe(100);
   });
 });
